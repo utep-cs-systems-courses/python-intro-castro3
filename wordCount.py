a691e2bfc866d756f959e5c4cb820dad65f285c9
castro3 <castroxoscar@gmail.com>
2020-08-31 06:16:23 -0600

    Updated word count

--- wordCount.py
+++ wordCount.py
@@ -1,1 +1,24 @@
 #Oscar Castro
+
+import re 
+import sys
+
+# set the input and output files
+inputFname = sys.argv[1]
+outputFname = sys.argv[2]
+
+with open(inputFname, 'r') as i:
+    lines = i.read()
+    
+list = re.findall("\S+", lines)
+
+wordList = {}
+
+for word in list:
+    if word not in wordList:
+        wordList[word] = 0
+    wordList[word] =+ 1
+
+with open(outputFname,'w') as o:
+    for word, times in sorted(wordList.items()):
+        o.write(word + " " + str(times) + "\n")
